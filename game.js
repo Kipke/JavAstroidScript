838614132de34ad2a08190b3cf4b4fa8e923ed84
Mikuso <g_github@flamescape.com>
2014-10-31 21:08:51 +0000

    loop de doop

--- game.js
+++ game.js
@@ -1,9 +1,18 @@
 
 
+function processInput() {
+}
 
+function processPhysics() {
+}
+
+function draw() {
+}
 
 function gameLoop() {
-    
+    processInput();
+    processPhysics();
+    draw();
 }
 
 setInterval(gameLoop, 60/1000);
