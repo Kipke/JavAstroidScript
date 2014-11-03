b2b2618cd0f3e1664b90703bbc717b6a4e597665
Kipke <bjorn.molenmaker@gmail.com>
2014-11-03 23:40:36 +0100

    Added draw
    see title

--- controller.js
+++ controller.js
@@ -23,8 +23,6 @@ function processInput(world) {
 		if(world.player.mov.length() > world.player.maxSpeed){
 			world.player.mov.scale(world.player.maxSpeed / world.player.mov.length());
 		}
-
-
 	}
 	if (keysHeld[32]) {
 		// shoot
@@ -38,6 +36,11 @@ function processInput(world) {
 function processPhysics(world) {
 	// move forward
 	world.player.pos.translate(world.player.mov);
+	// apply the drag
+	if(world.player.mov.length() > 0){
+		//world.player.mov.move(-0.05,(Math.PI *2) - world.player.angle);
+		world.player.mov.scale(0.99);
+	}
 	// update the player charge
 	if(world.player.charge < world.player.fireRate){
 		world.player.charge++;
